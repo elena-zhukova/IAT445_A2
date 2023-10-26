6edd31b0e7f38b7f66026a56eb965d602c3e0a6b
elena-zhukova <lena.nik.2000@gmail.com>
2023-10-26 03:58:29 -0700

    Particle Systems done

---
+++ Assets/Scripts/Destruct.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Destruct : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Trigger");
+        if (collision.gameObject.tag.Equals("DestructTrigger"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
